5885980a4a42dcc76ae45ca8c14f850d418c3231
sarwirr <sarrah.benhalima@insat.ucar.tn>
2024-03-23 17:11:05 +0100

    moving backend repository

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { BaseModule } from './base/base.module';
+import * as dotenv from 'dotenv';
 
+dotenv.config()
 @Module({
-  imports: [],
+  imports: [ 
+    MongooseModule.forRoot(
+      `mongodb://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_URI}:${process.env.DB_PORT}/${process.env.DB_NAME}?authSource=admin` 
+      ), BaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
