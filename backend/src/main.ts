b8cf510a7cf85abfaeddeda22ba45d39f46e8d0a
sarwirr <sarrah.benhalima@insat.ucar.tn>
2024-05-28 21:48:38 +0200

    enable cors to front

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,7 +7,13 @@ import { HttpExceptionFilter } from './common/filter/http-exception/http-excepti
 dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  
+  app.enableCors({
+    origin: 'http://localhost:5173', // Specify the allowed origin
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+  
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new HttpExceptionFilter());
 
