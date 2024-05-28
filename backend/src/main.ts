bed8978158bbff3a859e8bed1acc0839d10b73d0
sarwirr <sarrah.benhalima@insat.ucar.tn>
2024-05-28 22:11:47 +0200

    enable cors erased options

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -8,11 +8,7 @@ dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
-  app.enableCors({
-    origin: 'http://localhost:5173', // Specify the allowed origin
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: true,
-  });
+  app.enableCors();
   
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new HttpExceptionFilter());
